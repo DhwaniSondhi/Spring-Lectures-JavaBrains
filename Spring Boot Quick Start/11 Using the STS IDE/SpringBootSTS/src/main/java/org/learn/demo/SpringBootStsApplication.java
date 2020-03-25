3c467a8f48fcd5f5a9eca8d3d0a8883a8744f640
DSondhi <dhwanisondhi3@gmail.com>
2020-03-25 07:12:28 -0400

    Using the STS IDE Lecture

---
+++ Spring Boot Quick Start/11 Using the STS IDE/SpringBootSTS/src/main/java/org/learn/demo/SpringBootStsApplication.java
@@ -0,0 +1,13 @@
+package org.learn.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootStsApplication.class, args);
+	}
+
+}
---
+++ Spring Boot Quick Start/11 Using the STS IDE/SpringBootSTS/src/test/java/org/learn/demo/SpringBootStsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learn.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootStsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
